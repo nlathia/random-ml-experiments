d98500ca6f5d9b4deffee2dda16d0874b0111c7b
nlathia <neal.lathia@cl.cam.ac.uk>
2016-03-01 12:39:17 +0000

    Add constants

---
+++ bnp/dataset.py
@@ -0,0 +1,13 @@
+from os import path
+
+TARGET_CLASS = 'target'
+
+DATA_PATH = '/Users/neal/Desktop/kaggle-experiments/bnp/data'
+
+FULL_TRAIN_FILE = path.join(DATA_PATH, 'train.csv')
+FULL_TEST_FILE = path.join(DATA_PATH, 'test.csv')
+SUBMISSION_FILE = path.join(DATA_PATH, 'sample_submission.csv')
+
+SUBSET_TRAIN_FILE = path.join(DATA_PATH, 'subset-training.csv')
+SUBSET_TEST_FILE = path.join(DATA_PATH, 'subset-testing.csv')
+
