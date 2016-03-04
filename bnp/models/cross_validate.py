92914aae14677c2e01c956afb0080466e3ca4325
nlathia <neal.lathia@cl.cam.ac.uk>
2016-03-04 12:51:16 +0000

    Store only new features

--- bnp/models/cross_validate.py
+++ bnp/models/cross_validate.py
@@ -11,7 +11,7 @@ import numpy as np
 import dataset
 
 
-def load_file(data_file):
+def load_file(data_file, new_feature_files):
     """
     Loads some data and splits into X, y
     :param data_file: Full path to CSV file
@@ -23,26 +23,35 @@ def load_file(data_file):
     X.drop(dataset.ROW_ID, axis=1, inplace=True)
     y = data[dataset.TARGET_CLASS]
     row_ids = data[dataset.ROW_ID]
+
+    for new_feature_file in new_feature_files:
+        print '\tRead', new_feature_file
+        new_features = pd.read_csv(new_feature_file)
+        X = pd.concat([X, new_features], axis=1)
     return X, y, row_ids
 
 
-def load_subset(directory, train_file, test_file):
+def load_subset(directory, feature_files):
     """
     Loads a subset of the data
     :param directory: The subset's location
+    :param feature_files: Any extra feature files
     :return: training Xs and test ys
     """
-    X_train, y_train, _ = load_file(path.join(directory, train_file))
-    X_test, y_test, row_ids = load_file(path.join(directory, test_file))
+    train_features = [path.join(directory, f + dataset.TRAIN_FILE) for f in feature_files]
+    test_features = [path.join(directory, f + dataset.TEST_FILE) for f in feature_files]
+
+    X_train, y_train, _ = load_file(path.join(directory, dataset.TRAIN_FILE), train_features)
+    X_test, y_test, row_ids = load_file(path.join(directory, dataset.TEST_FILE), test_features)
     return X_train, X_test, y_train, y_test, row_ids
 
 
-def cross_validate(name, model, train_file, test_file):
+def cross_validate(name, model, features=[]):
     losses = []
     for i in xrange(0, 10):
         print 'Subset', i
         directory = path.join(dataset.DATA_PATH, dataset.SUBSET + str(i))
-        X_train, X_test, y_train, y_test, row_ids = load_subset(train_file, test_file)
+        X_train, X_test, y_train, y_test, row_ids = load_subset(directory, features)
 
         print '\tTrain', name
         model.fit(X_train, y_train)
@@ -59,9 +68,9 @@ def cross_validate(name, model, train_file, test_file):
 
 
 if __name__ == '__main__':
-    TRAIN = 'corr-0.9train.csv'
-    TEST = 'corr-0.9test.csv'
 
+    name = 'corr-0.9'
+    features = ['corr-0.9']
     models = {
         'LogisticRegression': LogisticRegression(),
         'RandomForest-500-gini': RandomForestClassifier(n_estimators=500, criterion='gini', n_jobs=-1),
@@ -73,5 +82,5 @@ if __name__ == '__main__':
     with open('corr-0.9-result.csv', 'w') as out:
         rows = writer(out)
         for n, m in models.iteritems():
-            avg, std = cross_validate(n, m, TRAIN, TEST)
-            rows.writerow([TRAIN, n, avg, std])
+            avg, std = cross_validate(n, m, features)
+            rows.writerow([name, n, avg, std])
--- bnp/preprocess/features.py
+++ bnp/preprocess/features.py
@@ -33,22 +33,19 @@ def add_correlated_differences(alpha=0.5):
                     new_feature = '-'.join(sorted([a, b]))
                     if new_feature not in corrs:
                         corrs.add(new_feature)
-                        sparse_b = train[b][train[b] != -1]
-                        corr = sparse_a.corr(sparse_b)  # min_periods?
-                        if corr > alpha:
-                            print a, b, corr
+                        corr = sparse_a.corr(train[b][train[b] != -1])  # min_periods?
+                        if corr >= alpha:
+                            print '\t', a, b, corr
                             new_train[new_feature] = abs(train[a] - train[b])
                             new_test[new_feature] = abs(test[a] - test[b])
 
         print 'New columns', len(new_train.columns)
-        result = pd.concat([train, new_train], axis=1)
-        result.to_csv(path.join(directory, 'corr-' + str(alpha) + dataset.TRAIN_FILE))
-
-        result = pd.concat([test, new_test], axis=1)
-        result.to_csv(path.join(directory, 'corr-' + str(alpha) + dataset.TEST_FILE))
+        new_train.to_csv(path.join(directory, 'corr-' + str(alpha) + dataset.TRAIN_FILE))
+        new_test.to_csv(path.join(directory, 'corr-' + str(alpha) + dataset.TEST_FILE))
 
 
-add_correlated_differences()
+add_correlated_differences(alpha=0.9)
+add_correlated_differences(alpha=0.5)
 
 
 
